0f903d96f9495eb52f9a05a2862bb9791a6c04f5
Kacper <kacper.gajko1@gmail.com>
2024-02-02 17:05:55 +0100

    added spinning animation

--- src/routes/WheelOfFortune.svelte
+++ src/routes/WheelOfFortune.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	let wheel: any;
 	let wheelCanvas: any;
 	let slice: any;
-	
-	let x=0;
+
+	let x = 0;
 	let numberOfSlices = 5;
-	let player = [70, 40, 35, 15,20];
+	let player = [70, 40, 35, 15, 20];
 	let totalSilver = 0;
-	for(x=0;x<numberOfSlices;x++){
+	for (x = 0; x < numberOfSlices; x++) {
 		totalSilver += player[x];
-	};
-	const kolory =["red","blue","green"] //kolory kawałów
+	}
+	const kolory = ['red', 'blue', 'green']; //kolory kawałów
 
 	for (x = 0; x < numberOfSlices; x++) {
 		player[x] = player[x] / totalSilver; //procent szans :)
@@ -29,18 +30,32 @@
 		ctx.moveTo(400, 400);
 	};
 
-	x=0; //zmienna do kawałów
+	x = 0; //zmienna do kawałów
 	let ileKulkaPelne = 0; //zmienna do kawałów
 	const drawSlice = (sliceCtx: any) => {
-		for(x; x<numberOfSlices;x++){
-		sliceCtx.beginPath();
-		sliceCtx.arc(400, 400, 300, ileKulkaPelne, ileKulkaPelne += player[x] * Math.PI * 2); //nadanie lie kula pelne wartości zalełnionego koła
-		sliceCtx.lineTo(400, 400);
-		sliceCtx.fillStyle = kolory[x%3]
-		sliceCtx.fill();
-		sliceCtx.stroke();
+		for (x; x < numberOfSlices; x++) {
+			sliceCtx.beginPath();
+			sliceCtx.arc(400, 400, 300, ileKulkaPelne, (ileKulkaPelne += player[x] * Math.PI * 2)); //nadanie lie kula pelne wartości zalełnionego koła
+			sliceCtx.lineTo(400, 400);
+			sliceCtx.fillStyle = kolory[x % 3];
+			sliceCtx.fill();
+			sliceCtx.stroke();
 		}
 	};
+
+	function SpinWheel() {
+		let min = 10;
+		let max = 30;
+		let degree = Math.floor(Math.random() * (max - min) * 1000);
+		slice.style.transition = 'all 10s ease-out';
+		slice.style.transform = `translate(-50%, 0%) rotate(${degree}deg)`;
+
+		setTimeout(() => {
+			slice.style.transition = 'none';
+			let actualDeg = degree % 360;
+			slice.style.transform = `translate(-50%, 0%) rotate(${actualDeg}deg)`;
+		}, 10000);
+	}
 
 	onMount(() => {
 		console.log('Onmount');
@@ -51,10 +66,11 @@
 	});
 </script>
 
-<div class="WheelOfFortune">
-	<canvas bind:this={wheelCanvas} width={canvas.width} height={canvas.height} />
-		<canvas bind:this={slice} width={canvas.width} height={canvas.height} />
+<div class="WheelOfFortune" bind:this={wheel}>
+	<canvas id="wheelCanvas" bind:this={wheelCanvas} width={canvas.width} height={canvas.height} />
+	<canvas id="slice" bind:this={slice} width={canvas.width} height={canvas.height} />
 </div>
+<button on:click={SpinWheel}></button>
 
 <style lang="scss">
 	.WheelOfFortune {
