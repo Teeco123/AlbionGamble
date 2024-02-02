a5a2c3462147030511e0b13fe4c688c45d992688
Kacper <kacper.gajko1@gmail.com>
2024-02-02 16:00:27 +0100

    added db insert balance 0

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,19 +1,21 @@
-import { db } from "$lib/server/db.js";
-import { redirect } from "@sveltejs/kit";
+import { db } from '$lib/server/db.js';
+import { redirect } from '@sveltejs/kit';
 
 export const actions = {
 	login: async ({ request, cookies }) => {
 		const data = await request.formData();
-		const login = data.get("login");
-		const password = data.get("password");
+		const login = data.get('login');
+		const password = data.get('password');
 
-		let user = await db.collection("Users").findOne({ login: { $eq: login }, password: { $eq: password } });
+		let user = await db
+			.collection('Users')
+			.findOne({ login: { $eq: login }, password: { $eq: password } });
 
 		const userid = String(user?._id);
 
 		if (user) {
-			cookies.set("session", userid, {
-				path: "/",
+			cookies.set('session', userid, {
+				path: '/',
 				httpOnly: true,
 				secure: true,
 				maxAge: 60 * 60 * 24 * 2
@@ -31,13 +33,13 @@ export const actions = {
 
 	register: async ({ request }) => {
 		const data = await request.formData();
-		const login = data.get("login");
-		const password = data.get("password");
+		const login = data.get('login');
+		const password = data.get('password');
 
-		let user = await db.collection("Users").findOne({ login: login });
+		let user = await db.collection('Users').findOne({ login: login });
 
 		if (!user && password && login) {
-			db.collection("Users").insertOne({ login: login, password: password });
+			db.collection('Users').insertOne({ login: login, password: password, balance: 0 });
 			return { success: true };
 		} else if (!login) {
 			return { loginRequired: true };
