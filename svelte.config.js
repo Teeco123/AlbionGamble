b2cb03dee143dcfaff45a9638c6d785c25f7b84a
Kacper <kacper.gajko1@gmail.com>
2024-02-04 03:29:44 +0100

    fixed issue with vercel adapter & firebase and sveltefire version conflict

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/vercel-adapter';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
