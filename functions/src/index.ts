b3c2f15f2af6113ae8e07085701eac3cbf75cd3c
Kacper <kacper.gajko1@gmail.com>
2024-02-19 19:50:01 +0100

    Cloud function to create a gamble document every minute

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,25 @@
 import { onRequest } from 'firebase-functions/v2/https';
+import { onSchedule } from 'firebase-functions/v2/scheduler';
+import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp } from 'firebase-admin/app';
 import * as logger from 'firebase-functions/logger';
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
 export const helloWorld = onRequest((request, response) => {
 	logger.info('Hello logs!', { structuredData: true });
 	response.send('Hello from Firebase!');
+});
+
+export const createGamble = onSchedule('* * * * *', async () => {
+	const serverTime = new Date().toUTCString();
+
+	let randomDegree = Math.floor(Math.random() * (50 - 10) * 1000);
+
+	await getFirestore().collection('gambles').add({
+		date: serverTime,
+		totalPlayers: 0,
+		totalSilver: 0,
+		spinDegree: randomDegree
+	});
 });
